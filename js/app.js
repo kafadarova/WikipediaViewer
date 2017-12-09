5bd2fcdf4dc32d659f905c3524b1ef44e812a7cd
kafadarova <k.kafadarova@gmail.com>
2017-12-09 12:04:46 +0100

    Ajax Request changed + some styling added (the results after search)

--- js/app.js
+++ js/app.js
@@ -1,29 +1,66 @@
-//building Wikipedia full url
-function build_wiki_search_url(searchTerm) {
-var base_url = "https://en.wikipedia.org/w/api.php";
-var format = "&format=json";
-var request_url = "?action=query&format=json&list=search&srsearch=";
-var url = base_url + request_url + searchTerm;
-return url;
-}
+//creating a variable for the new content after the search
+var pages = $('.wrap');
+
+
+$( document ).ready(function() {
+	$("#result").click(function(e){
+    e.preventDefault();
+    // value entered by the user
+   var searchTerm = $("#search").val();
+   console.log(searchTerm);
+   // url to look for using the search input by the user
+   var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+ searchTerm + "&format=json&callback=?";
 
 // AJAX call
-$( document ).ready(function() {
-	$("#search").click(function(e){
-    e.preventDefault();
-   var searchTerm = $("#search").val();
-   var url = build_wiki_search_url(searchTerm);
-		$.ajax({
+    $.ajax({
       type:'GET',
       url:  url,
-      dataType: 'jsonp',
+      async:true,
+      data: { action: 'query', list: 'search', srsearch: searchTerm, format: 'json' },
+      dataType: "jsonp",
       success: function(data) {
         console.log(data);
-      var result = data.query.pages;
-      for (var num in result) {
-             $("ul").append('<a target="_blank" href="https://en.wikipedia.org/wiki?curid=' + result[num].pageid + '">' + '<li><h3 class="title ">' + result[num].title + '</h3><p class="text">' + result[num].extract + '</p></li></a>');
-           }
+        var html  = '  <!-- pages -->';
+            html += '  <div class="row row-centered">';
+
+        data.query.search.map(function(w) {
+          html += '    <div class="col-centered">';
+          html += '      <a href="https://en.wikipedia.org/wiki/' + w.title + '" target="_blank">';
+          html += '        <div class="panel panel-default">';
+          html += '          <div class="panel-heading">';
+          html += '            <h3 class="panel-title">' + w.title + '</h3>';
+          html += '          </div>';
+          html += '          <div class="panel-body">';
+          html += '            ' + w.snippet;
+          html += '          </div>';
+          html += '        </div>';
+          html += '      </a>';
+          html += '    </div>';
+        });
+
+    html += '  </div>';
+
+ // changing the content after the result - displaying all possible result pages
+        pages.html(html);
       }
-    });
-  });
+     });
+     });
 });
+
+// $(".searchbox").autocomplete({
+//     source: function(request, response) {
+//         console.log(request.term);
+//         $.ajax({
+//             url: "http://en.wikipedia.org/w/api.php",
+//             dataType: "jsonp",
+//             data: {
+//                 'action': "opensearch",
+//                 'format': "json",
+//                 'search': request.term
+//             },
+//             success: function(data) {
+//                 response(data[1]);
+//             }
+//         });
+//     }
+// });
